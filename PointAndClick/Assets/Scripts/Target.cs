bc7cd27872fb4fc409e83c3e980c7f79ad66a3af
Matthew <maa92498@gmail.com>
2020-09-02 17:52:39 -0400

    Simple point, Click, Destroy
    The target circle will follow mouse and when box is clicked the box destroys.

---
+++ PointAndClick/Assets/Scripts/Target.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        if (Input.GetMouseButtonDown(0))
+		{
+            Destroy(gameObject);
+		}
+    }
+}
